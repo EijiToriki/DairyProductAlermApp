185796c4e9adca8f32a8b6720fafd4b2e7bfad40
Eiji Toriki <ageForEngi@outlook.jp>
2024-01-19 22:07:43 +0900

    ER図、クラス図作成。フロントとバックエンドの初期設定

---
+++ 30_backend/itemManagement/src/test/java/dairy/product/itemManagement/ItemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dairy.product.itemManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
