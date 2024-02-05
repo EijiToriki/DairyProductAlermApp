c9b7af896cbbc729ff7d33c3dec9abc44954d735
Eiji Toriki <ageForEngi@outlook.jp>
2024-02-05 22:57:44 +0900

    フロントエンド toppageの画面作成

--- 20_frontend/src/component/Sidebar.js
+++ 20_frontend/src/component/Sidebar.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../css/Sidebar.css"
-import { NavLink, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 export const Sidebar = () => {
   const navigate = useNavigate()
+  const [btnNum, setBtnNum] = useState(1)
 
-  const handlePageTransition = (pageName) => {
+  const handlePageTransition = (pageName, btnNum) => {
+    setBtnNum(btnNum)
     navigate(pageName)
   }
 
   return (
     <div className='sidebar'>
-      <button className='buttonOutline' onClick={() => handlePageTransition("/top")} >TOPへ</button>
-      <button className='buttonOutline' onClick={() => handlePageTransition("/view")}>日用品一覧</button>
-      <button className='buttonOutline' onClick={() => handlePageTransition("/register")}>日用品登録</button>
+      {
+        btnNum === 1 ?
+        <button className='buttonOutlineRed' disabled='true'>TOPへ</button>
+        :
+        <button className='buttonOutline' onClick={() => handlePageTransition("/top", 1)} >TOPへ</button>
+      }
+
+      {
+        btnNum === 2 ?
+        <button className='buttonOutlineRed' disabled='true'>日用品一覧</button>
+        :
+        <button className='buttonOutline' onClick={() => handlePageTransition("/view", 2)}>日用品一覧</button>
+      }
+
+      {
+        btnNum === 3 ?
+        <button className='buttonOutlineRed' disabled='true'>日用品登録</button>
+        :
+        <button className='buttonOutline' onClick={() => handlePageTransition("/register", 3)}>日用品登録</button>
+      }
     </div>
   )
 }
--- 20_frontend/src/component/TopPage.js
+++ 20_frontend/src/component/TopPage.js
@@ -1,8 +1,84 @@
 import React from 'react'
 import "../css/TopPage.css"
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 export const TopPage = () => {
+  const total_price = 5500
+  const price_diff = 700
+
+  const cardStyle = {
+    width: '30%', // カードの横幅
+    height: '250px', // カードの高さ
+  };
+
   return (
-    <div>TopPage</div>
+    <div className='toppage'>
+      <div className='statistics_this_month'>
+        <div className='statistics_sent'>
+          今月は日用品に 
+        </div>
+        <div className='price'>
+            {total_price} 
+        </div>
+        <div className='statistics_sent'>
+          円使う予定です
+        </div>
+      </div>
+
+      <div className='statistics_compare'>
+        <div className='statistics_sent'>
+          (前月より 
+        </div>
+        {
+        price_diff > 0 ? 
+          <>
+            <div className='price_blue'>
+              {price_diff} 
+            </div>
+            <div className='statistics_sent'>
+              円 DOWN)
+            </div>
+          </>
+        :
+          <>
+            <div className='price_red'>
+              {-price_diff} 
+            </div>
+            <div className='statistics_sent'>
+              円 UP)
+            </div>
+          </>
+        }
+      </div>
+
+      <div className='info'>
+        <div className='info_text'>
+          １週間以内に買足しが必要な日用品
+        </div>
+        <div className='cards'>
+          <Card style={cardStyle}>
+            <CardContent>
+              
+            </CardContent>
+          </Card>
+          <Card style={cardStyle}>
+            <CardContent>
+              {/* カードのコンテンツ */}
+            </CardContent>
+          </Card>
+          <Card style={cardStyle}>
+            <CardContent>
+              {/* カードのコンテンツ */}
+            </CardContent>
+          </Card>
+        </div>
+        
+
+      </div>
+
+
+      
+    </div>
   )
 }
