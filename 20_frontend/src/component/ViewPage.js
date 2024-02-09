e8fb9b1ed15ad7478b817c0a2cd99085396f0399
Eiji Toriki <ageForEngi@outlook.jp>
2024-02-09 21:41:15 +0900

    日用品一覧　検索機能の実装

--- 20_frontend/src/component/ViewPage.js
+++ 20_frontend/src/component/ViewPage.js
@@ -20,6 +20,10 @@ const cardStyle = {
 
 export const ViewPage = () => {
   const [allItems, setAllItems] = useState([])
+  const [filteredItems, setFilteredItems] = useState([])
+  const [word, setWord] = useState("")
+  const [tag, setTag] = useState("")
+  const [sortMode, setSortMode] = useState("")
 
   useEffect(() => {
     const params = {
@@ -28,39 +32,108 @@ export const ViewPage = () => {
     const get_all_data = async(params) => {
       const res = await axios.get("http://localhost:8080/all_items", {params})
       setAllItems(res.data)
+      setFilteredItems(res.data)
     }
     get_all_data(params)
   }, [])
 
+  const handleChange = (e) => {
+    const search_word = e.target.value
+    setWord(search_word) 
+    setTag("")
+    if(search_word === ""){
+      setFilteredItems(allItems)
+    }else{
+      setFilteredItems(allItems.filter(item => {
+        return item.name.includes(search_word);
+      }))
+    }
+  }
+
+  const handleTagSelect = (e) => {
+    const search_tag = e.target.value
+    setWord("")
+    setTag(search_tag)
+    setFilteredItems(allItems.filter(item => {
+      return item.tag === search_tag;
+    }))
+  }
+
+  const handleSort = (e) => {
+    const sort_mode = e.target.value
+    setSortMode(sort_mode)
+
+    if(sort_mode === "五十音順"){
+      setFilteredItems(
+        filteredItems.sort((a, b) => {
+          return a.name.localeCompare(b.name, 'ja');
+        })
+      )
+    }else if(sort_mode === "五十音順（逆）"){
+      setFilteredItems(
+        filteredItems.sort((a, b) => {
+          return b.name.localeCompare(a.name, 'ja');
+        })
+      )
+    }else if(sort_mode === "買足し期限が短い順"){
+    }else if(sort_mode === "買足し期限が長い順"){
+    }else if(sort_mode === "値段が高い順"){
+      setFilteredItems(
+        filteredItems.sort((a, b) => {
+          return parseInt(b.price) - parseInt(a.price);
+        })
+      )
+    }else if(sort_mode === "値段が安い順"){
+      setFilteredItems(
+        filteredItems.sort((a, b) => {
+          return parseInt(a.price) - parseInt(b.price);
+        })
+      )
+    }
+  }
+
+
+
   return (
     <div>
       <div className='search_field'>
-        <TextField id="outlined-basic" label="検索ワード" variant="outlined" style={{ width: '35%'}} />
+        <TextField 
+          id="outlined-basic" 
+          label="検索ワード" 
+          variant="outlined" 
+          onChange={handleChange}
+          style={{ width: '35%'}} 
+          value={word}
+        />
         <FormControl style={{ width: '30%'}}>
           <InputLabel id="tag">タグ</InputLabel>
           <Select
+            value={tag}
             labelId="tag"
             id="tag"
             label="タグ"
+            onChange={handleTagSelect}
           >
-            <MenuItem value={10}>風呂</MenuItem>
-            <MenuItem value={20}>台所</MenuItem>
-            <MenuItem value={30}>おしゃれ</MenuItem>
+            <MenuItem value={'風呂'}>風呂</MenuItem>
+            <MenuItem value={'台所'}>台所</MenuItem>
+            <MenuItem value={'おしゃれ'}>おしゃれ</MenuItem>
           </Select>
         </FormControl>
         <FormControl style={{ width: '30%'}}>
           <InputLabel id="sort">ソート順</InputLabel>
           <Select
+            value={sortMode}
             labelId="sort"
             id="sort"
             label="ソート順"
+            onChange={handleSort}
           >
-            <MenuItem value={10}>五十音順</MenuItem>
-            <MenuItem value={20}>五十音順（逆）</MenuItem>
-            <MenuItem value={30}>買足し期限が短い順</MenuItem>
-            <MenuItem value={40}>買足し期限が長い順</MenuItem>
-            <MenuItem value={50}>値段が高い順</MenuItem>
-            <MenuItem value={60}>値段が安い順</MenuItem>
+            <MenuItem value={"五十音順"}>五十音順</MenuItem>
+            <MenuItem value={"五十音順（逆）"}>五十音順（逆）</MenuItem>
+            <MenuItem value={"買足し期限が短い順"}>買足し期限が短い順</MenuItem>
+            <MenuItem value={"買足し期限が長い順"}>買足し期限が長い順</MenuItem>
+            <MenuItem value={"値段が高い順"}>値段が高い順</MenuItem>
+            <MenuItem value={"値段が安い順"}>値段が安い順</MenuItem>
           </Select>
         </FormControl>
       </div>
@@ -71,12 +144,12 @@ export const ViewPage = () => {
         </div>
         <div className='cards'>
           {
-            allItems.length === 0 ?
+            filteredItems.length === 0 ?
               <div className='nothing_notice'>
                 ないです
               </div>
             :
-              allItems.map((item, idx) => (
+              filteredItems.map((item, idx) => (
                 <Card style={cardStyle} key={idx}>
                   <CardContent className='cardcontent'>
                     <div className='product_title'>
