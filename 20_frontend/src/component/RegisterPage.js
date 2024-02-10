de9092baef69a2224c35aae3751eed405e928a8e
Eiji Toriki <ageForEngi@outlook.jp>
2024-02-10 21:57:12 +0900

    日用品登録　画面作成

--- 20_frontend/src/component/RegisterPage.js
+++ 20_frontend/src/component/RegisterPage.js
@@ -1,7 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "../css/RegisterPage.css"
+import { FormControl, InputLabel, Select, TextField, MenuItem } from '@mui/material';
 
 export const RegisterPage = () => {
+  const [image, setImage] = useState(null);
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setImage(selectedImage);
+  };
+
+
   return (
-    <div>RegisterPage</div>
+    <div className='upload_form'>
+      <div className='upload_img'>
+        <label htmlFor="fileInput" className="image_label">
+          <div className='label_container'>
+            画像ファイルを選択
+          </div>
+        </label>
+        <input id="fileInput" type="file" accept="image/*" onChange={handleImageChange} style={{ display: 'none' }} />
+      </div>
+      <div className='item_form'>
+        <div className='item_name'>
+          <TextField 
+            id="outlined-basic" 
+            label="日用品名" 
+            variant="outlined" 
+            style={{ width: '50%'}} 
+          />
+        </div>
+
+        <div className='item_tag'>
+          <TextField 
+            id="outlined-basic" 
+            label="タグ" 
+            variant="outlined" 
+            style={{ width: '50%'}} 
+          />
+        </div>
+
+        <div className='item_price'>
+          <TextField 
+            id="outlined-basic" 
+            label="価格" 
+            variant="outlined" 
+            style={{ width: '50%'}} 
+          />
+        </div>
+
+        <div className='item_span'>
+          <TextField 
+            id="outlined-basic" 
+            label="買足し期間" 
+            variant="outlined" 
+            style={{ width: '35%'}} 
+          />
+          <FormControl style={{ width: '15%'}}>
+            <InputLabel id="unit">単位</InputLabel>
+            <Select
+              labelId="unit"
+              id="unit"
+              label="タグ"
+            >
+              <MenuItem value={'日'}>日</MenuItem>
+              <MenuItem value={'月'}>月</MenuItem>
+              <MenuItem value={'年'}>年</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className='button_position'>
+          <button className='buttonOutline'>登録</button>
+        </div>
+
+
+        
+      
+      </div>
+    </div>
   )
 }
